90ecadc9db7626b865b65c1271f41fc0b7009227
Zsombinszky <zsombi.studio@gmail.com>
2025-10-04 03:19:08 +0200

    starter code ready

---
+++ README.md
@@ -0,0 +1,11 @@
+```json
+"top_speed": {
+  "value": 191,
+  "units": "km/s",
+  "approx_kmh": 687600
+}
+```
+
+- `"value": 191` → a számérték maga, vagyis **191**
+- `"units": "km/s"` → megmondja, hogy ez **kilométer per szekundumban** értendő
+- `"approx_kmh": 687600` → ugyanaz az érték átszámolva **kilométer/órára**
---
+++ script.js
@@ -0,0 +1,37 @@
+const fileName = "data.json";
+
+const colors = [
+  "coral",
+  "tomato",
+  "orange",
+  "gold",
+  "limegreen",
+  "mediumseagreen",
+  "turquoise",
+  "deepskyblue",
+  "dodgerblue",
+  "royalblue",
+  "blueviolet",
+  "mediumorchid",
+  "fuchsia",
+  "deeppink",
+  "crimson",
+  "red",
+  "orangered",
+  "springgreen",
+  "aqua",
+  "chartreuse",
+];
+
+const readData = async (fileName) => {
+  try {
+    const res = await fetch(fileName);
+    const json = await res.json();
+    return json;
+  } catch (error) {
+    console.error("Error reading file", error.message);
+    return null;
+  }
+};
+
+const rootElement = document.getElementById("root");
